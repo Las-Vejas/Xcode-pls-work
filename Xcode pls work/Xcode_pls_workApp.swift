89c41abf9905fe7ee08278cac87812752c1a0313
Las-Vejas <vejas.sereicikas@icloud.com>
2025-06-05 10:22:00 +0200

    Initial Commit

---
+++ Xcode pls work/Xcode_pls_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_pls_workApp.swift
+//  Xcode pls work
+//
+//  Created by Vejas Sereicikas on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_pls_workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
